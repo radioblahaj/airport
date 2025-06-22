50ae6386a2734265b46592f91bed66a37a765db9
Arav Narula <arav.1tb@gmail.com>
2025-06-22 16:37:51 -0400

    changes

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 import airportData from 'airport-data-js';
-import { parse } from 'dotenv';
+import chalk from 'chalk';
 
 async function fetchAirportData(airportCode) {
     try {
-        const airport = await airportData.getAirportByIata(airportCode)
+        const airport = await airportData.getAirportByIata(airportCode);
         const data = airport[0];
         const { latitude, longitude, wikipedia_link, flightradar24_url } = data;
         console.log(latitude, longitude, wikipedia_link, flightradar24_url);
         return { latitude, longitude, wikipedia_link, flightradar24_url };
-
     } catch (error) {
         console.error('Error fetching airport data:', error);
     }
 }
 
-const airportCode = 'YYZ'
-const airportInformation = await fetchAirportData(airportCode)
-
+const airportCode = 'YYZ';
+const airportInformation = await fetchAirportData(airportCode);
 
 async function getTrackingArea(airportInformation) {
-    console.log(airportInformation)
-    const trackingArea = []
+    console.log(airportInformation);
+    const trackingArea = [];
     const { latitude, longitude } = airportInformation;
-    const delta = 0.2
+    const delta = 0.2;
     const lat = parseFloat(latitude);
     const lon = parseFloat(longitude);
     const minLat = (lat - delta).toFixed(4);
@@ -35,7 +33,7 @@ async function getTrackingArea(airportInformation) {
         maxLat,
         minLon,
         maxLon
-    }
+    };
 }
 
 async function getFlightsData(minLat, maxLat, minLon, maxLon) {
@@ -45,23 +43,21 @@ async function getFlightsData(minLat, maxLat, minLon, maxLon) {
             maxLat: maxLat,
             minLon: minLon,
             maxLon: maxLon
-        }
-
-
+        };
 
         const url = `https://opensky-network.org/api/states/all?lamin=${data.minLat}&lomin=${data.minLon}&lamax=${data.maxLat}&lomax=${data.maxLon}`;
-        console.log(url);
+        const response = await fetch(url);
+        const overhead = await response.json();
+        console.log(overhead);
+        console.log(chalk.red(url));
     } catch (error) {
         console.error('Error fetching flights data:', error);
     }
 }
+
 const { minLat, maxLat, minLon, maxLon } = await getTrackingArea(airportInformation);
-getFlightsData(minLat, maxLat, minLon, maxLon);
+const getData = await getFlightsData(minLat, maxLat, minLon, maxLon);
 
-/*
-1. Fetch airport data by IATA code
-2. Extract latitude, longitude, Wikipedia link, and Flightradar24 URL
-3. Get the boundary box of the airport
-4. Return the data
-*/
 
+
+export { fetchAirportData, getTrackingArea, getFlightsData };
