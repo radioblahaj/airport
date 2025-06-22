19519cc3a5c447c3c16c55572b249563764e5007
Arav Narula <arav.1tb@gmail.com>
2025-06-22 17:02:37 -0400

    Enhance getFlightRadarData function to log detailed flight information and return aircraft type

--- index.js
+++ index.js
@@ -54,22 +54,30 @@ async function getFlightsData(minLat, maxLat, minLon, maxLon) {
     }
 }
 
-async function getFlightRadarURL(callsign) {
+async function getFlightRadarData(callsign) {
     try {
         const url = `https://www.flightradar24.com/v1/search/web/find?query=${callsign}`;
         const response = await fetch(url)
         const data = await response.json()
+        // console.log(data)
         const flightRadarID = data.results[1].id
+        const {ac_type, logo, reg, flight} = data.results[1].detail;
+        console.log(chalk.bgCyanBright(`Aircraft Type: ${ac_type}`));
+        console.log(chalk.bgBlueBright(`Flight Code: ${flight}`));
+        console.log(chalk.bgYellowBright(`Registration: ${reg}`));
+        console.log(chalk.bgGreenBright(`Logo: ${logo}`));
+        console.log(chalk.bgRedBright(`Flight Radar ID: ${flightRadarID}`));
+        console.log(chalk.bgMagentaBright(`Callsign: ${callsign}`));
         const flightRadarURL = `https://www.flightradar24.com/${callsign}/${flightRadarID}`;
-        console.log(chalk.green(flightRadarURL));
-        return flightRadarURL;
+        console.log(chalk.bgWhiteBright(flightRadarURL));
+        return flightRadarURL, ac_type;
     }
     catch (error) {
         console.error('Error fetching FlightRadar URL:', error);
     }
 }
 
-getFlightRadarURL('KLM204')
+getFlightRadarData('KLM204')
 
 const { minLat, maxLat, minLon, maxLon } = await getTrackingArea(airportInformation);
 const getData = await getFlightsData(minLat, maxLat, minLon, maxLon);
